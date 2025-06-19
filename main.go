355936289ba2cb618e235bf9fc2a4c46a4a11748
Газимов Диас <gvzumdias@gmail.com>
2025-06-19 15:42:05 +0500

    it is my first problem solving code

--- main.go
+++ main.go
@@ -1,8 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func Changename(mir []int) {
+	mir[1] = 12
+
+}
+
+// func plusOne(digits []int) []int {
+// 	for i := len(digits) - 1; i >= 0; i-- {
+// 		if digits[i] < 9 {
+// 			digits[i]++
+// 			return digits
+// 		}
+// 		digits[i] = 0
+// 	}
+
+// 	// если все цифры были 9, например [9, 9] → [1, 0, 0]
+// 	return append([]int{1}, digits...)
+// }
+
+func addBinary(a string, b string) string {
+	result_of_a := 0
+	result_of_b := 0
+	power_of_a := 1
+	power_of_b := 1
+	var chars []byte
+	for i := len(a) - 1; i >= 0; i-- {
+		if a[i] == '0' || a[i] == '1' {
+			bit := int(a[i] - '0')
+			result_of_a += power_of_a * bit
+			power_of_a *= 2
+		}
+	}
+
+	for i := len(b) - 1; i >= 0; i-- {
+		if b[i] == '0' || b[i] == '1' {
+			bit := int(b[i] - '0')
+			result_of_b += power_of_b * bit
+			power_of_b *= 2
+		}
+	}
+	sum_of_var := result_of_a + result_of_b
+
+	for sum_of_var > 0 {
+		digit := (sum_of_var % 2)
+		char := byte('0' + digit)
+		chars = append([]byte{char}, chars...)
+		sum_of_var /= 2
+	}
+	return string(chars)
+
+}
 func main() {
-	variable4, variable5 := "multiple", 4
-	fmt.Println(variable4, variable5)
+	a := "0"
+	b := "0"
+	fmt.Println(addBinary(a, b))
 }
